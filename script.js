b3fffb2fa2b6317aedb60e9e1ab6f158ffb6b366
Ozan <47722606+oynozan@users.noreply.github.com>
2023-07-24 16:35:44 +0300

    Turkish to English

--- script.js
+++ script.js
@@ -3,21 +3,7 @@ let startInput = false;
 let wordLen = 0;
 let wrongWords = 0;
 
-const words = [
-"armut", "elma", "şeker", "bilgisayar",
-"yazılım", "star", "sevgi", "aşk", "araba",
-"bellek", "bina", "tuğla", "defter", "kitap",
-"tel", "direnç", "kulaklık", "monitör", "vizyon",
-"galip","savaş","barış","tüp","el","uzunluk","karpuz",
-"kavun","yaz","kış","anne","klavye","tabak","şarap",
-"bardak","kablo","mendil","renk","gökkuşağı",
-"poster","oyun", "sevgili", "güzel", "çirkin",
-"kız","erkek","oyuncu","dert","tas","ekran","kılavuz",
-"öğretmen","öğrenci","türkiye","parfüm","takvim","diş",
-"cetvel","pergel","açı","sabit","değişken","matematik",
-"fizik","coğrafya","edebiyat","içeri","dışarı","makas",
-"eldiven","peçete","fare","kedi"
-];
+const words = ["no","thing","well","because","also","two","use","tell","good","first","man","day","find","give","more","new","one","us","any","those","very","her","need","back","there","should","even","only","many","really","work","life","why","right","down","on","try","let","something","too","call","woman","may","still","through","mean","after","never","no","world","in","feel","yeah","great","last","child","oh","over","ask","when","as","school","state","much","talk","out","keep","leave","put","like","help","big","where","same","all","own"];
 
 const tdCount = $("td").length;
 for (let i = 0; i < tdCount; i++) {
@@ -27,12 +13,13 @@ for (let i = 0; i < tdCount; i++) {
 }
 
 function result() {
-	let wps = (wordLen)/60
+	let wps = (wordLen / 60).toFixed(2);
+	
 	$(".result").html(`
-	<p>Dakikada: ${counter} Kelime</p>
-	<p>Saniyede: ${wps} Kelime</p>
-	<p>Yanlış Yazılan Kelime Sayısı: ${wrongWords} Kelime</p>
-	`);
+	<p>${counter} Words per Minute</p>
+	<p>${wps} Words per Second</p>
+	<p>Typo count: ${wrongWords} word</p>`);
+	
 	$(".word-input").attr("disabled","disabled");
 }
 
@@ -46,12 +33,8 @@ function clock(sec,min) {
 			clearInterval(timer);
 			result();
 		}
-		if (minPart == -1) {
-			minPart = 0;
-		}
-		if (secPart == -1) {
-			secPart = 0;
-		}
+		if (minPart == -1) minPart = 0;
+		if (secPart == -1) secPart = 0;
 		if (secPart != -1 && minPart != -1) {
 			if (minPart.toString().length == 1) {
 				minZero = "0";
@@ -64,10 +47,8 @@ function clock(sec,min) {
 				secZero = "";
 			}
 		}
-		$(".clock").text(
-		minZero+minPart.toString()+":"+secZero+secPart.toString()
-		);
-	},1000);
+		$(".clock").text(minZero+minPart.toString()+":"+secZero+secPart.toString());
+	}, 1000);
 }
 
 function confirm(x) {
@@ -89,20 +70,18 @@ function confirm(x) {
 
 function input() {
 	if (!startInput) {
-		clock(0,1); //MAX SEC:59 MAX MIN:59
+		clock(0,1); // MAX SEC:59 MAX MIN:59
 		startInput = true;
 	}
 	let data = $("input").val();
-	if (data.search(" ") !== -1) {
-		confirm(data)
-	}
+
+	if (data.search(" ") !== -1) confirm(data)
 	else if (data != "") {
 		if ($("td:eq(0)").text().startsWith(data) === false) {
 			$("td:eq(0)").css({"background-color":"red","border-bottom":"red"});
 		} else {
-			$("td:eq(0)").css({"background-color":"lime","border-bottom":"lime"});
+			$("td:eq(0)").css({"background-color":"#02c81e","border-bottom":"#02c81e"});
 		}
-	} else {
-		$("td:eq(0)").css({"background-color":"#292929","border-bottom":"lime"});
 	}
+	else $("td:eq(0)").css({"background-color":"#292929","border-bottom":"#02c81e"});
 }
